364a25da8e3c25e4b3fb5863e8902c47dc37508f
U2tzJTNE <u2tzjtne@gmail.com>
2021-04-07 09:51:58 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/u2tzjtne/flutter_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.u2tzjtne.flutter_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/startup_name.dart';
+
+//入口
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(primaryColor: Colors.white),
+        home: new Scaffold(
+          appBar: new AppBar(title: new Text('Flutter Demo')),
+          body: new ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: [
+              ListItem(
+                title: 'startup_name',
+                screenPage: new RandomWords(),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ListItem extends StatefulWidget {
+  const ListItem({
+    this.title,
+    this.screenPage,
+  });
+
+  final String title;
+  final Widget screenPage;
+
+  @override
+  _ListItemState createState() => _ListItemState();
+}
+
+class _ListItemState extends State<ListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      title: new Text(widget.title),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => widget.screenPage),
+        );
+      },
+    );
+  }
+}
